673973f2b146504853902ab89bffa157bea18091
vickrasov <vic.krasov@gmail.com>
2015-12-25 20:25:55 +0200

    Початкова версія

---
+++ modules/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru-ru">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <title>Сайт</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="/css/more.css" type="text/css" />
+    <link rel="stylesheet" href="/css/default.css" type="text/css" />
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700&subset=latin,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://cdn.devpl.net/v1.1/index.js"></script>
+    <script type="text/javascript" src="/js/my.js"></script>
+    <link rel="icon" type="image/ico" href="/favicon.ico" />
+	<script type="text/javascript" src="/js/toastr.js"></script>
+	<link rel="stylesheet" href="/css/toastr.css">
+</head>
