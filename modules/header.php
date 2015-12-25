06b1e5d2639aa62a60468a689794fcb59a144b8a
TeamWeb4u <info@web4u.in.ua>
2015-12-25 23:35:10 +0200

    Зминив header.php

--- modules/header.php
+++ modules/header.php
@@ -11,9 +11,7 @@
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700&subset=latin,cyrillic-ext,cyrillic' rel='stylesheet' type='text/css'>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="http://cdn.devpl.net/v1.1/index.js"></script>
+    <script type="text/javascript" src="/js/index.js"></script>
     <script type="text/javascript" src="/js/my.js"></script>
     <link rel="icon" type="image/ico" href="/favicon.ico" />
-	<script type="text/javascript" src="/js/toastr.js"></script>
-	<link rel="stylesheet" href="/css/toastr.css">
 </head>
